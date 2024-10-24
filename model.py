f6fb7a2f60fe1ea03eebf5c067ff0a94fb76467f
natethedequate <white.n@northeastern.edu>
2024-10-24 13:36:49 -0400

    extracted swin_transformer

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch
-from SwinTransformer.models.swin_transformer_v2 import SwinTransformerV2
+from swin_transformer import SwinTransformer
 from constants import IMG_SIZE
 
 class DepthAndUncertaintyModel(torch.nn.Module):
@@ -8,16 +8,20 @@ class DepthAndUncertaintyModel(torch.nn.Module):
     
     def forward(self, x):
         print("Entering forward...")
-        swint = SwinTransformerV2(
-            img_size = IMG_SIZE,
-            depth
+        # args from https://github.com/SwinTransformer/Swin-Transformer-Semantic-Segmentation/blob/main/configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py
+        swint = SwinTransformer(
+            embed_dim=96,
+            depths=[2, 2, 6, 2],
+            num_heads=[3, 6, 12, 24],
+            window_size=7,
+            ape=False,
+            drop_path_rate=0.3,
+            patch_norm=True,
+            use_checkpoint=False
 
         )
         x = swint(x)
         return x
-
-
-
 
 def signature(rgb: torch.tensor) -> torch.tensor:
     """
--- newsmoketest.py
+++ newsmoketest.py
@@ -1,7 +1,10 @@
-from constants import IMG_SIZE
-from mmseg.models.backbones import SwinTransformer
-import numpy as np
 import torch
+
+
+from constants import IMG_SIZE
+from swin_transformer import SwinTransformer
+import numpy as np
+
 
 x = SwinTransformer()(torch.tensor([
         [[[5.0]*IMG_SIZE]*IMG_SIZE,[[4.0]*IMG_SIZE]*IMG_SIZE,[[3.0]*IMG_SIZE]*IMG_SIZE]
