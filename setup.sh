6c9d2182c76bce2b62da904aeffcb04ca32d4821
natethedequate <white.n@northeastern.edu>
2024-10-24 14:06:33 -0400

    restore setup

--- setup.sh
+++ setup.sh
@@ -9,31 +9,15 @@
 #SBATCH --mem=16GB
 
 # Commands to execute
-module load anaconda3/2019.10
-# module load gcc/9.2.0
-# module load cuda/11.3
+module load anaconda3/2022.05
+module load gcc/10.1.0
+module load cuda/11.8
 
 echo 'btw, gpu:t4:1 seems to work if the v100-sxm2 is not available'
 
-conda create -n cs4100 python=3.8 -y
+conda create -n cs4100 python=3.12
 
-conda activate cs4100
+source activate cs4100
 
-# conda install pytorch=1.11.0 torchvision -c pytorch
-
-pip install -U openmim
-mim install mmcv-full
-pip install mmsegmentation
-
-# source activate cs4100
-
-# pip install torch torchvision torchaudio numpy --index-url https://download.pytorch.org/whl/cu118
-
-# # Model deps
-
-# # https://github.com/open-mmlab/mmsegmentation/blob/master/docs/en/get_started.md
-# pip install -U openmim
-# mim install mmcv-full
-# pip install mmsegmentation
-# export CUDA_HOME=/usr/local/cuda-10.2
-
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install timm configargparse h5py
