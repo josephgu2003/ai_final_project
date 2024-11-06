04eb1b78dc8cfd269a2ee35ef9671462f4716d89
natetheadequate <natewhite345@gmail.com>
2024-11-06 14:15:47 -0500

    untested local changes

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import torch
+import numpy
+
 from dataloader import BatchedImages
 from swin_transformer import SwinTransformer
-from constants import IMG_SIZE
+from constants import IMG_SIZE, DECODER_LAYER_CT
 
 class DepthAndUncertaintyModel(torch.nn.Module):
     def __init__(self):
@@ -16,14 +18,19 @@ class DepthAndUncertaintyModel(torch.nn.Module):
             drop_path_rate=0.3,
             patch_norm=True,
             use_checkpoint=False
-
         )
-        self.layer_one = torch.nn.Linear(768,2)
+        BACKBONE_OUT_DEPTH = 768
+        BACKBONE_OUT_SIZE = 16
+        self.decoder_layer_depths = np.linspace(BACKBONE_OUT_DEPTH,2, num=DECODER_LAYER_CT+1)
+        self.decoder_linear_layers = [torch.nn.Linear(self.decoder_layer_depths[i],self.decoder_layer_depths[i+1]) for i in range(DECODER_LAYER_CT)]
+        self.decode_interp_sizes = np.linspace(BACKBONE_OUT_SIZE, IMG_SIZE, DECODER_LAYER_CT)
     
     def forward(self, x: BatchedImages):
         x = self.backbone(x.rgb)
         x = x[-1] # torch.Size([16, 768, 20, 15])
-        x= torch.nn.functional.interpolate(x, mode="bilinear", size=IMG_SIZE)
-        x = x.permute(0,2,3,1)
-        x = self.layer_one(x)
+        for i in range(DECODER_LAYER_CT):
+            x = x.permute(0,2,3,1)
+            x= self.decoder_linear_layers[i](x)
+            x = x.permute(0,3,1,2)
+            x= torch.nn.functional.interpolate(x, mode="bilinear", size=self.decode_interp_sizes[i])
         return x
