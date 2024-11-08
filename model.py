33ba05411eeb4430b2eca05fb0072bb83114c78c
Joseph Gu <josephgu2003@gmail.com>
2024-11-08 08:58:15 -0500

    rough multiscale decoder and increase epochs

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ from dataloader import BatchedImages
 from swin_transformer import SwinTransformer
 from constants import IMG_SIZE
 
+import torch.nn.functional as F
+
 class DepthAndUncertaintyModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -18,12 +20,30 @@ class DepthAndUncertaintyModel(torch.nn.Module):
             use_checkpoint=False
 
         )
-        self.layer_one = torch.nn.Linear(768,2)
+        self.layer_one = torch.nn.Linear(768,384)
+        self.layer_two = torch.nn.Linear(384,192)
+        self.layer_three = torch.nn.Linear(192,2)
     
+    def apply_linear(self, linear, x):
+        return linear(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
     def forward(self, x: BatchedImages):
-        x = self.backbone(x.rgb)
-        x = x[-1] # torch.Size([16, 768, 20, 15])
+        out = self.backbone(x.rgb)
+        x = out[-1] # torch.Size([16, 768, 20, 15])
+
+        x = F.relu(self.apply_linear(self.layer_one, x))
+
+        x= torch.nn.functional.interpolate(x, mode="bilinear", size=(30, 40))
+
+        x += out[-2]
+
+        x = F.relu(self.apply_linear(self.layer_two, x))
+
+        x= torch.nn.functional.interpolate(x, mode="bilinear", size=(60, 80))
+
+        x += out[-3]
+
+        x = self.apply_linear(self.layer_three, x)
+
         x= torch.nn.functional.interpolate(x, mode="bilinear", size=IMG_SIZE)
-        x = x.permute(0,2,3,1)
-        x = self.layer_one(x)
-        return x
+    
+        return x.permute(0, 2, 3, 1)
