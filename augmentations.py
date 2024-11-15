e8f92acaf520123302dceea80bb148355997b5cd
natethedequate <white.n@northeastern.edu>
2024-11-14 22:19:22 -0500

    augmentations rough draft

--- augmentations.py
+++ augmentations.py
@@ -7,27 +7,33 @@ A collection of functions which:
 
     Also contains a compose function for composing them
 """
-
-
 import torch
-from torchvision.transforms.v2 import RandomHorizontalFlip, RandomChannelPermutation
+import torchvision.transforms.v2 as T
 from PIL.Image import Image
 from typing import Callable
-import random
+from random import random
 
-def horizontal_flip(image: Image, label: Image, p=0.5) -> tuple[torch.Tensor,torch.Tensor]:
-    random_horizontal_flip = RandomHorizontalFlip(p)
-    return random_horizontal_flip(image, label)
+def horizontal_flip(p):
+    res = T.RandomHorizontalFlip(p)
+    return res, res
 
-def channel_shift(image: Image, label: Image, p):
-    """Shifts color channel randomly"""
-    random.choice(range(3))
-    r = RandomChannelPermutation()
-    return r(image, label)
+def channel_shift(p):
+    return (T.RandomChannelPermutation() if random() < p else T.Identity()), T.Identity()
 
-def compose(img: Image, label: Image, transforms: Callable[[Image, Image],tuple[torch.Tensor,torch.Tensor]]):
-    img = img
-    label = label
-    for transform in transforms:
-        img, label = transform(img, label)
-    return img,label
+def cutout(p):
+    res = T.Compose(T.PILToTensor(), T.RandomErasing(p), T.ToPILImage())
+    return res, res
+
+def affine(max_angle, max_translate_pct, fill_color):
+    res = T.RandomAffine(degrees=max_angle, translate=(max_translate_pct,max_translate_pct), fill=fill_color)
+    return res, res
+
+def grayscale(p):
+    return T.RandomGrayscale(p), T.Identity()
+
+def compose(transforms: Callable[[Image, Image],tuple[torch.Tensor,torch.Tensor]]):
+    img_t, label_t = map(T.Compose, zip(transforms))
+    def res(img, label):
+        return img_t(img), label_t(label)
+    return res
+
