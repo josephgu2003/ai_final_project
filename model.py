57abb508101a782d0b726c154ffb1c9d43c0290a
Joseph Gu <josephgu2003@gmail.com>
2024-11-19 20:28:47 -0500

    upgrade model with monte carlo dropout

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 from dataloader import BatchedImages
+from layers import AttnDecoderBlock, ConvDecoderBlock, MCDropout
 from swin_transformer import SwinTransformer, SwinTransformerBlock
 from constants import IMG_SIZE
 
@@ -20,38 +21,23 @@ class DepthAndUncertaintyModel(torch.nn.Module):
             use_checkpoint=False
 
         )
-        self.conv_one = torch.nn.Conv2d(768, 384, kernel_size=(3,3),padding=1)
-        # window size 20 means it is like a full attention layer
-        self.attn = SwinTransformerBlock(dim=384, num_heads=6, window_size=20, attn_drop=0.3) # will accept a [384, 20, 15] input after reshaping
-        self.attn.H = 15
-        self.attn.W = 20
-
-        self.conv_two = torch.nn.Conv2d(384, 192, kernel_size=(3,3),padding=1)
-        self.conv_three = torch.nn.Conv2d(192, 2, kernel_size=(3,3),padding=1)
         
-    def apply_linear(self, linear, x):
-        return linear(x)
-    
+        self.attn_one = AttnDecoderBlock(768, 384, 30, 40)
+        self.conv_two = ConvDecoderBlock(384, 192, 60, 80)
+        self.conv_three = ConvDecoderBlock(192, 96, 120, 160)
+        self.conv_four = ConvDecoderBlock(96, 2, IMG_SIZE[0], IMG_SIZE[1], act=torch.nn.Identity)
+        
+        self.mc_dropout = MCDropout(p=0.5)
+        
     def forward(self, x: BatchedImages):
         out = self.backbone(x.rgb)
         x = out[-1] # torch.Size([16, 768, 20, 15])
-
-        x = F.relu(self.apply_linear(self.conv_one, x))
-
-        x = self.attn(x.flatten(-2).permute(0, 2, 1), mask_matrix=None).permute(0, 2, 1).reshape(x.shape)
-
-        x= torch.nn.functional.interpolate(x, mode="bilinear", size=(30, 40))
-
-        x += out[-2]
-
-        x = F.relu(self.apply_linear(self.conv_two, x))
-
-        x= torch.nn.functional.interpolate(x, mode="bilinear", size=(60, 80))
-
-        x += out[-3]
-
-        x = self.apply_linear(self.conv_three, x)
-
-        x= torch.nn.functional.interpolate(x, mode="bilinear", size=IMG_SIZE)
+        x = self.attn_one(x)
+        x = self.mc_dropout(x)
+        x = self.conv_two(x, out[-2])
+        x = self.mc_dropout(x)
+        x = self.conv_three(x, out[-3])
+        x = self.mc_dropout(x)
+        x = self.conv_four(x, out[-4])
     
         return x.permute(0, 2, 3, 1)
